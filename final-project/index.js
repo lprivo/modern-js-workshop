5eb3adb28a93e5a39feba008ceb388808d68acf5
Gyula Soos <gyula.soos@aerian.com>
2019-03-08 15:13:07 +0000

    import network and moviedata and make search work

--- final-project/index.js
+++ final-project/index.js
@@ -1,2 +1,14 @@
-import { OMDB_API_KEY } from "./credentials";
-console.log(OMDB_API_KEY);
+import { findSomeMoviePosters } from "./movieData";
+
+const searchResults = document.getElementById("search-results");
+const searchTextField = document.getElementById("search-movies");
+const searchButton = document.getElementById("search-button");
+
+const addPosterBricks = async () => {
+    const searchTerm = searchTextField.value;
+    const moviePosters = await findSomeMoviePosters(searchTerm);
+
+    searchResults.innerHTML = moviePosters.join("");
+};
+
+searchButton.onclick = addPosterBricks;
---
+++ final-project/movieData.js
@@ -0,0 +1,17 @@
+import { OMDB_API_KEY } from "./credentials";
+import { getSomeData } from "./network";
+
+export const hasAPoster = (movie) => movie.Poster !== "N/A";
+
+export const getPoster = (movie) =>
+    `<img src="${movie.Poster}" alt="${movie.Title}" />`;
+
+export const findSomeMoviePosters = async (key) => {
+    const result = await getSomeData(
+        `http://www.omdbapi.com/?s=${key}&apikey=${OMDB_API_KEY}`
+    );
+
+    const posters = result.Search.filter(hasAPoster).map(getPoster);
+
+    return posters;
+};
