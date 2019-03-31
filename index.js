2e1e122c62ee6bf7723af296c5e9b8a2f4f1f713
Gyula Soos <gyula.soos@aerian.com>
2019-03-31 23:28:45 +0100

    add pagination

--- index.js
+++ index.js
@@ -5,9 +5,17 @@ import { debounce } from "./utils";
 const searchResults = document.getElementById("search-results");
 const errorField = document.getElementById("errors");
 const searchTextField = document.getElementById("search-movies");
-const paginator = document.getElementById("paginator");
-let currentPage = 1;
-let thereIsMore = false;
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+
+const state = {
+    currentPage: 1,
+    maxPage: 2,
+};
+
+const setMaxPage = (results) => {
+    state.maxPage = Math.ceil(results / 10);
+};
 
 const setErrorMessage = (errorCode, errorMessage) => {
     if (errorCode !== "200") {
@@ -19,9 +27,12 @@ const setErrorMessage = (errorCode, errorMessage) => {
 
 const getMovies = async () => {
     const searchTerm = searchTextField.value;
-    const movies = await findMovies(searchTerm, currentPage);
+    const movies = await findMovies(searchTerm, state.currentPage);
+    const { ResponseCode, Error, totalResults } = movies;
 
-    setErrorMessage(movies.ResponseCode, movies.Error);
+    setMaxPage(totalResults);
+
+    setErrorMessage(ResponseCode, Error);
 
     return movies;
 };
@@ -47,43 +58,36 @@ const addEventListeners = () => {
     });
 };
 
-const debouncedGetMovies = debounce(() => {
-    getMovies()
-        .then(fillScreenWithPosters)
-        .then(addEventListeners);
-}, 500);
-
-const goToPage = (page = 1) => {
-    currentPage += page;
-    getMovies()
-        .then(fillScreenWithPosters)
-        .then(addEventListeners);
-};
-
-// doesn't work yet
-const addPaginator = () => {
-    const pageButtons = {};
-
-    if (currentPage > 1) {
-        pageButtons.previousButton = `<div id="previous-page">Previous Page</div>`;
-    }
-
-    if (thereIsMore) {
-        console.log("thereIsMore", thereIsMore);
-
-        pageButtons.nextButton = `<div id="next-page">Next Page</div>`;
-    }
-    paginator.innerHTML = Object.values(pageButtons).join("");
-
-    document.getElementById("previous-page").onClick(goToPage);
-    document.getElementById("next-page").onClick(goToPage);
-};
+const debouncedGetMovies = (immediate) =>
+    debounce(
+        () => {
+            getMovies()
+                .then(fillScreenWithPosters)
+                .then(addEventListeners);
+        },
+        500,
+        immediate
+    );
 
 searchTextField.onkeyup = (event) => {
     event.preventDefault();
-    debouncedGetMovies();
-    // addPaginator();
+    debouncedGetMovies()();
 };
 
-// add pagination
+const changeCurrentPage = (page) => {
+    const { currentPage, maxPage } = state;
+
+    if (page + currentPage >= 1 && page + currentPage <= maxPage) {
+        state.currentPage += page;
+        // get movies instantly with true flag
+        debouncedGetMovies(true)();
+    }
+
+    console.log("currentPage", currentPage);
+    console.log("maxpage", maxPage);
+};
+
+prev.onclick = () => changeCurrentPage(-1);
+next.onclick = () => changeCurrentPage(1);
+
 // style the results
